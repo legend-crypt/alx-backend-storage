aa7392d7422a78a666bac7ddb06075ea3629d774
lord <konadulordkweku@gmail.com>
2024-09-16 11:09:41 +0000

    Refactor table creation syntax in 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,9 @@
 --- Create table'
---- columns id, email, name, country
+--- columns id, email, name
 
 CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NUlL UNIQUE,
     name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-)
+);
