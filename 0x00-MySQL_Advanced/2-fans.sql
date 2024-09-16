689cbc61dce46bba8b2dfee23f2d662618ff97b0
lord <konadulordkweku@gmail.com>
2024-09-16 11:16:34 +0000

    Refactor table creation syntax in 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,8 @@
 --- Write a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
+--- Context: Calculate/compute something is always power intensive… better to distribute the load!
+--- Column names must be: origin and nb_fans
+
+
 
 
 SELECT origin, SUM(fans) nb_fans 
