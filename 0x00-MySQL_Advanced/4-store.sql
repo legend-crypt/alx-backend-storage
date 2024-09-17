f7a2989ba3073ea08b7b36e0a5b155326003d088
lord <konadulordkweku@gmail.com>
2024-09-17 23:27:47 +0000

    Refactor trigger in 4-store.sql to use case-insensitive search for item name

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -6,6 +6,6 @@ AFTER INSERT ON orders FOR EACH ROW
 BEGIN 
      UPDATE items
      SET quantity = quantity - NEW.number
-     WHERE name = NEW.item_name;
+     WHERE NAME = NEW.item_name;
 END $$
 DELIMITER ;
