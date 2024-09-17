0be24cf90093b396ebb47de37eba2db8249b0f45
lord <konadulordkweku@gmail.com>
2024-09-17 23:14:25 +0000

    Add trigger to decrease item quantity after order insertion

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,3 +1,5 @@
+--- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+
 DELIMITER $$
 CREATE TRIGGER IF NOT EXISTS after_order_insertion 
 AFTER INSERT ON orders FOR EACH ROW
