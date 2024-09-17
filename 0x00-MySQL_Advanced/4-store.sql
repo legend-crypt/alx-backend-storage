cef8e2829ea269f3dff2dbc282e99f3650776fc3
lord <konadulordkweku@gmail.com>
2024-09-17 23:29:05 +0000

    Refactor trigger in 4-store.sql to use case-insensitive search for item name

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,11 +1,7 @@
---- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+-- Order store procedure
 
-DELIMITER $$
-CREATE TRIGGER IF NOT EXISTS after_order_insertion 
-AFTER INSERT ON orders FOR EACH ROW
-BEGIN 
-     UPDATE items
-     SET quantity = quantity - NEW.number
-     WHERE NAME = NEW.item_name;
-END $$
-DELIMITER ;
+
+CREATE TRIGGER decrement AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
