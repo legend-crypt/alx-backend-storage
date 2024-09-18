fc7107c23e6c601a2fa6cf6cae1c29457fc4a952
lord <konadulordkweku@gmail.com>
2024-09-18 00:29:29 +0000

    Add index on 'name' column to optimize queries by initial character and score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,1 +1,5 @@
+
+-- This SQL script creates an index named 'idx_name_first_score' on the 'names' table.
+-- The index is created on the first character of the 'name' column and the entire 'score' column.
+-- Indexes can improve the performance of queries that filter or sort by these columns.
 CREATE INDEX idx_name_first_score on names(name(1), score);
