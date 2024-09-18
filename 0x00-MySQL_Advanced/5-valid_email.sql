ff9bf80ea9fcde9e6d495b9f39b6c8e6b4f54fe4
lord <konadulordkweku@gmail.com>
2024-09-18 00:04:35 +0000

    Refactor trigger in 5-valid_email.sql to correctly reset valid_email attribute when email is changed

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 CREATE TRIGGER resetemail
 BEFORE UPDATE ON users FOR EACH ROW
 BEGIN 
-    IF OLD.email != NEW.email THEN
+    IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
 END $$
